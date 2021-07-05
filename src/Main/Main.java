c2d74506b8cfe2cc45c6728155d7e33589452008
adrian <goformusicro@gmail.com>
2021-07-05 13:28:24 +0200

    Initial commit

---
+++ src/Main/Main.java
@@ -0,0 +1,18 @@
+
+package Main;
+
+import View.FairyTaleApp;
+import javafx.application.Application;
+
+/**
+ * The Main.Main class is used the start the program
+ * @author Adrian Militaru, Adrian Pompierescu, Gabriel Moutinho Tristan, Freja Hansen
+ * @version 1.0
+ */
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Application.launch(FairyTaleApp.class, args);
+    }
+}
---
+++ src/View/FairyTaleApp.java
@@ -0,0 +1,27 @@
+package View;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+
+public class FairyTaleApp extends Application {
+    /**
+     * An abstract method which automatically calls the launch
+     * @param window setting the stage
+     * @throws IOException exception thrown if an IO error occurs
+     */
+    public void start(Stage window) throws IOException
+    {
+        window.setTitle("Fairy Tale Lib");
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(getClass().getResource("FairyTaleGUI.fxml"));
+        Scene scene = new Scene(loader.load());
+        window.setResizable(false);
+        window.setScene(scene);
+        window.show();
+    }
+}
